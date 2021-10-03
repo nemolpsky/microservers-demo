a257bfcbd114f35c188b7ac531cd754c1eb31688
nemolpsky <git@github.com:nemolpsky/Note.git>
2021-10-03 18:46:38 +0800

    add gateway code and configure example.

--- gateway/src/main/java/com/lp/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/lp/gateway/GatewayApplication.java
@@ -1,13 +1,26 @@
 package com.lp.gateway;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@Slf4j
 public class GatewayApplication {
+
+    @Value("${gateway.message:can not load configure.}")
+    private String message;
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+    @Bean
+    public void print() {
+        log.info("gateway message:{}", message);
+    }
 }
--- gateway/src/main/java/com/lp/gateway/LoggingFilter.java
+++ gateway/src/main/java/com/lp/gateway/LoggingFilter.java
@@ -1,25 +1,30 @@
 package com.lp.gateway;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
 import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR;
 
+/**
+ * 这个日志过滤器只有在进行转发的时候才会走到，也就是说如果请求没有正确的在配置文件中配置，这里是不打印任何东西的。
+ */
 @Component
+@Slf4j
 public class LoggingFilter implements GlobalFilter {
-    private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
 
-
+    @SneakyThrows
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        logger.info("1:{}.",exchange.getRequest().toString());
-        logger.info("2:{}.",exchange.getRequest().getPath());
-        logger.info("3:{}.",exchange.getAttribute(GATEWAY_ROUTE_ATTR).toString());
+        log.info("1. localAddress:{}.",exchange.getRequest().getLocalAddress());
+        log.info("2. path:{}.",exchange.getRequest().getPath());
+        log.info("3. attr:{}.", Optional.ofNullable(exchange.getAttribute(GATEWAY_ROUTE_ATTR)).map(Object::toString).orElse("attr is empty."));
         return chain.filter(exchange);
     }
 }
